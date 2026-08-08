840eb1910441469a31cf28055f09b112cd3c4143
Hafi-grey <candypowers17@gmail.com>
2026-08-08 23:17:53 +0300

    Update WebSocket connection status messages

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 const status = document.getElementById("status");
+const tick = document.getElementById("tick");
 
-status.textContent = "Testing connection...";
+status.textContent = "Connecting...";
 
 const ws = new WebSocket(
     "wss://ws.derivws.com/websockets/v3?app_id=1089"
 );
 
 ws.onopen = function () {
-    status.textContent = "DERIV CONNECTION WORKS 🟢";
+
+    status.textContent = "Connected 🟢";
+
+    ws.send(JSON.stringify({
+        ticks: "R_100",
+        subscribe: 1
+    }));
+};
+
+ws.onmessage = function (event) {
+
+    const data = JSON.parse(event.data);
+
+    console.log(data);
+
+    if (data.error) {
+        status.textContent =
+            "ERROR: " + data.error.message;
+        return;
+    }
+
+    if (data.tick) {
+
+        status.textContent =
+            "LIVE TICK 🟢";
+
+        tick.textContent =
+            data.tick.quote;
+    }
 };
 
 ws.onerror = function () {
-    status.textContent = "DERIV CONNECTION FAILED 🔴";
+    status.textContent =
+        "WebSocket error 🔴";
 };
 
 ws.onclose = function () {
-    console.log("WebSocket closed");
+    console.log("Connection closed");
 };
