a651035ef5f86c8da63c34543053f0ba453d08df
Hafi-grey <candypowers17@gmail.com>
2026-08-09 13:43:28 +0300

    Rename variable 'strength' to 'status'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const status = document.getElementById("strength");
+const status = document.getElementById("status");
 alert("New scanner fields found!");
 const tick = document.getElementById("tick");
 const even = document.getElementById("even");
